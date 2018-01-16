a13c7498692b7b12db15766b23a6daa942ee76d1
johnnylasers <jianchen85@gmail.com>
2018-01-15 22:40:17 -0800

    search, post, elasticsearch, authentication

--- service/main.go
+++ service/main.go
@@ -4,8 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
-//	"log"
 	"strconv"
+	"log"
+	"reflect"
+	elastic "gopkg.in/olivere/elastic.v3" // alias is 'elastic'
+	"github.com/pborman/uuid"
+
+	"context"
+	"cloud.google.com/go/storage"
+
+	//"cloud.google.com/go/bigtable"
+
+	"io"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/go-redis/redis"
+
 )
 var m = map[string]string {
 	"fruit" : "apple",
@@ -20,36 +35,91 @@ type Location struct {
 
 type Post struct {
 	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
-	User     string `json : "user"`
-	Message  string  `json : "message"`
-	Location Location `json : "location"`
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
+	Url    string `json:"url"`
 }
 
 const (
-	DISTANCE = "200km"
+	INDEX = "around"
+	TYPE = "post"
+	DISTANCE = "10000km"
+	// Needs to update
+	PROJECT_ID = "around-186323"
+	BT_INSTANCE = "around-post-186323"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.227.186.151:9200/"
+	ENABLE_MEMCACHE = true
+	//ENABLE_BIGTABLE = false
+	REDIS_URL       = "redis-16659.c1.us-central1-2.gce.cloud.redislabs.com:16659"
+	BUCKET_NAME = "post-images-186323"
+
 )
 
+var mySigningKey = []byte("secret")
 
 func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+
+	fmt.Printf("exists = %v", exists)
+
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+                    "mappings":{
+                           "post":{
+                                  "properties":{
+                                         "location":{
+                                                "type":"geo_point"
+                                         }
+                                  }
+                           }
+                    }
+             }
+             `
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
 
 	//fmt.Println("started-service")
 	//http.HandleFunc("/search", handlerSearch)
 	//http.HandleFunc("/searchJSON", handlerSearchJSON)
 	//log.Fatal(http.ListenAndServe(":8081", nil))
+	fmt.Println("")
+	fmt.Println("started-service")
+	// Here we are instantiating the gorilla/mux router
+	r := mux.NewRouter()
 
-	//m = make(map[string]string) //make map, initialize and read to use
-	////m[“fruit”] = “apple”
-	////m[“person”] = “farmer”
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
 
-	for k, v := range m {
-		fmt.Printf("Key: %s --> Value: %s\n", k, v)
-	}
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
 
-	nums := [] int {2, 3, 4}
-	for i, num := range nums {
-		fmt.Printf("At index %s: %s\n", i, num)
-	}
-
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
@@ -60,14 +130,14 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
 	fmt.Println("Received one post request")
 
-	decoder := json.NewDecoder(r.Body)
+	//decoder := json.NewDecoder(r.Body)
+	//
 
-	var p Post //Post p = new Post();
-
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
-		return
-	}
+	//
+	//if err := decoder.Decode(&p); err != nil {
+	//	panic(err)
+	//	return
+	//}
 
 	// the code block above is equivalent to:
 	/*
@@ -78,58 +148,255 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	}
 	 */
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
-}
+	//id := uuid.New()
+	//// Save to ES.
+	//saveToES(&p, id)
+	//fmt.Fprintf(w, "Post received: %s\n", p.Message)
 
-func handlerSearch(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one search request")
-	//http://localhost:8080/search?lat=10.0&lon=20.0
-	Lat := r.URL.Query().Get("lat");
-	Lon := r.URL.Query().Get("lon");
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
 
 
-	fmt.Fprintf(w, "Search request received: Lat = %s & Lon = %s\n", Lat, Lon)
-}
+	// Codes below are for GCS
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
-func handlerSearchJSON(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one search JSON request")
 
-	//string converted to Float_64, would return 2 variable results
-	// _ in place of the second returned result, becuase we are confident we won't use it
-	// so we skipped naming it
-	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
 
-	rang := DISTANCE
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 
-	if val := r.URL.Query().Get("range"); val != "" {
-		rang = val + "km"
-	}
+	//debugging
+	fmt.Printf("lat %v lon %v \n", lat, lon)
 
-	fmt.Fprintf(w, "Search received: lat = %f ; lon = %f ; rang = %s\n", lat, lon, rang)
-
-	p := &Post {
-		User : "1111",
-		Message : "一生必去的100个地方",
-		Location : Location {
-			Lat : lat,
-			Lon : lon,
+	var p = Post{
+		//从username := claims.(jwt.MapClaims)["username"]取用户名，防止client假冒用户名
+		User: username.(string),
+		//User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
 		},
 	}
 
-	//js, err := json.Marshal(p); //把 p 拍扁然后写进一个json文本
-	if js, err := json.Marshal(p); err != nil {
-		panic(err)
-		return
-	} else {
+	id := uuid.New()
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
 	}
 
+	ctx := context.Background()
+
+	defer file.Close()
+	// replace it with your real bucket name.
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+	// Save to ES.
+	saveToES(&p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
 
 }
 
+func saveToGCS(ctx context.Context, r io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	// Student questions
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	bh := client.Bucket(bucket)
+	// Next check if the bucket exists
+	if _, err = bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bh.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
+
+}
+
+
+// Save a post to ElasticSearch
+func saveToES(p *Post, id string) {
+	// Create a client
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Save it to index
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+
+
+	// BigTable related codes
+/*
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	//column family name, column name,
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+*/
+
+}
+
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+
+	fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
+
+	key := r.URL.Query().Get("lat") + ":" + r.URL.Query().Get("lon") + ":" + ran
+	if ENABLE_MEMCACHE {
+		// TODO(student homework)
+		// 往redis set value
+		rs_client := redis.NewClient(&redis.Options{
+			Addr:     REDIS_URL,
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+
+		val, err := rs_client.Get(key).Result()
+		if err != nil {
+			fmt.Printf("Redis cannot find the key %s as %v.\n", key, err)
+		} else {
+			fmt.Printf("Redis find the key %s.\n", key)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(val))
+			return
+		}
+	}
+
+
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Define geo distance query as specified in
+	// https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	// Some delay may range from seconds to minutes. So if you don't get enough results. Try it later.
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(js)
+
+
+}
 
 
